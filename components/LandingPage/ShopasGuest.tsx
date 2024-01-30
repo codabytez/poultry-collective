cdda8f834f00088412248c78260ce5b6c6842f4e
codabytez <chidiobinna0001@gmail.com>
2024-01-30 19:11:58 +0100

    mdone seller responsive

--- components/LandingPage/ShopasGuest.tsx
+++ components/LandingPage/ShopasGuest.tsx
@@ -18,14 +18,16 @@ const ShopasGuest: NextPage = () => {
   return (
     <section
       id="shopAsGuest"
-      className="overflow-hidden mx-auto max-w-[1413px] px-5"
+      className={`overflow-hidden mx-auto max-w-[1413px] px-5 ${
+        allProducts.length > 0 && "hidden"
+      }`}
     >
       <div className="flex flex-col pb-1 items-start gap-10 my-32">
         <h1 className="text-H4-03 md:text-H2-03 text-cod-gray-cg-600">
           Shop as a Guest
         </h1>
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-[60px]">
-          {allProducts.length > 0 && !isLoading
+          {!isLoading
             ? allProducts
                 .slice(0, 6)
                 .map((product) => <Product key={product.$id} {...product} />)
